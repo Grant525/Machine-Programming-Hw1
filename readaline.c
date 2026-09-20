cdb26c55bcb8cb9c313f068f421c04c2088a90e6
willaandrade <91100629+willaandrade@users.noreply.github.com>
2026-09-20 12:54:35 -0400

    Add files via upload

--- readaline.c
+++ readaline.c
@@ -1,31 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 size_t readaline(FILE *inputfd, char **datapp){
-    (void) inputfd;
     //error checking 
     if (datapp == NULL){
         fprintf(stderr, "Runtime Error: readaline passed a NULL ptr\n");
         exit(EXIT_FAILURE);
     }
     //eof check
-    if(feof(inputfd)){
+    int c = fgetc(inputfd); 
+    if (c == EOF){
         *datapp = NULL;
         return 0;
     }
+    //put c back into input stream so it can be read later
+    ungetc(c, inputfd);
+
     //step 6: memory allocation + failure check
-    *datapp = malloc(1000 * sizeof(char)); //TODO: ask if this is acceptable re: idioms
-    if (datapp == NULL){
+    //TODO: ask if this is acceptable re: idioms
+    //TODO: change to Hanson new? but how would we tell it to make an arr of 
+    // size 1000
+    *datapp = malloc(1000 * sizeof(char)); 
+    if (*datapp == NULL){
         fprintf(stderr, "Runtime Error: memory allocation failed for datapp\n");
         exit(EXIT_FAILURE);
     }
+    
     //step 7: While loop to read in chars char by char
-    int c;
-    int num_chars = 0;
-    while ((c = fgetc(inputfd)) != EOF && num_chars < 1000){
-        printf("%c", (char) c);
+    int char_i = 0;
+    while ((c = fgetc(inputfd)) != '\n' && char_i < 999){
+        (*datapp)[char_i] = (char)c;
+        char_i++;
     }
-    printf("worked\n");
-    return 1;
+    //TODO: can we free memory past that \0 char? is it accessible? does it
+    //need to be?
+    //Step 8: add \0 char, and read through rest of oversize line if necessary
+    (*datapp)[char_i] = (char)'\0';
+    //printf("length of *datapp after adding backslash0 is %lu\n", strlen(*datapp));
+    if(char_i == 999){
+        while (c != '\n'){
+            (c = fgetc(inputfd));
+        }
+    }
+    //num_chars is one more than last index
+    return char_i;
 }
--- restoration.c
+++ restoration.c
@@ -1,23 +1,101 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "list.h"
+#include "mem.h"
 
 #include "readaline.h"
 //Function Declerations
 static FILE *open_or_abort(int argc, char *argv[]);
+void print_line(void **linepp, void*cl);
 
+struct line {
+    size_t length;
+    char *orig_cont;
+    char *clean_cont;
+    char *raw;
+    void *nondigits_atom;
+};
 
 int main(int argc, char *argv[]){
     FILE *fp = open_or_abort(argc, argv);
     char *datap;
+    
+    //Step 13: create Hanson list lines and push a line onto it
+    List_T lines = List_list(NULL);
+    
+    //Test: create a struct, set its length and orig_cont, call print_line on it
+    struct line *my_line;
+    NEW(my_line);
+    my_line->length = 5;
+    my_line->orig_cont = "hello";
+
+    void *my_line_vp = (void *)my_line;
+    printf("try calling print function on one line:\n"); 
+    print_line(&my_line_vp, NULL);
+
+   // printf("try mapping print function:\n");
+    List_push(lines, my_line_vp);
+    //testing step: does push work?
+    //try popping, pass List_pop a void ** to store address that will then 
+    //be set to point to the first thing that was in list, and now is popped.
+    //List_pop returns the list without the first elt. 
+    struct line *first_thing_p;
+    first_thing_p = malloc(sizeof(struct line *));
+
+    void *first_thing_vp = (void *)first_thing_p;
+    
+    lines = List_pop(lines, &first_thing_vp);
+    printf("The orig_cont of the line that was just popped from our list of \
+            lines is: %s\n", first_thing_p->orig_cont);
+
+    //List_map(lines, print_line, NULL);
+    
+
+    //Step 11
     int num_chars = readaline(fp, &datap);
-    printf("Readaline returned %d\n", num_chars);
-    if(datap == NULL){
-        printf("datap was set to NULL\n");
+    while(num_chars != 0){
+    //    printf("num of characters in the line:%d\n", num_chars);
+    //    printf("length of datap: %lu\n", strlen(datap));
+    //    printf("%s\n", datap);
+    //    printf("\nattempting to print out 999 bytes from datap\n");
+    //    printf("%.*s\n", 999, datap);
+        // struct line new_line;
+        // new_line.length = num_chars;
+        // new_line.orig_cont = datap;
+        // List_push(lines, (void *)&new_line);
+        
+        //last step: increment num_chars + datap
+        num_chars = readaline(fp, &datap);
     }
+    // printf("calling map\n");
+    // List_map(lines, print_line, NULL);
+    //  printf("called map\n");
+
+    //TODO: figure out what to do with this
+    // if(datap == NULL){
+    //     printf("datap was set to NULL\n");
+    // }
+    
     //free heap memory, close file
+    FREE(my_line);
     free(datap);
     fclose(fp);
     return EXIT_SUCCESS;
+}
+
+/* cont_deconstructor
+   Parameters: 
+*/
+
+
+/* print_line
+   Parameters: pointer to pointer to line struct (cast as void), NULL closure
+   Output: prints out the line's orig_cont
+*/
+void print_line(void **linepp, void*cl){
+    (void)cl;
+    printf("%s\n", ((struct line *)(*linepp))->orig_cont);
 }
 
 /* open_or_abort
@@ -43,12 +121,10 @@ static FILE *open_or_abort(int argc, char *argv[]){
                         "r");
                 exit(EXIT_FAILURE);
         }
-        printf("case 2\n");
     }
     if (argc == 1) {
         //Move file pointer to stdin
         fp = stdin;
-        printf("case 3\n");
     }
     return fp;
 }
